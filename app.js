20bdb24c1fe4dd7ddc70d010fda0cca979c0f983
sohail786jaffar <sohail.jaffar86@gmail.com>
2024-03-15 23:58:55 +0500

    m

---
+++ app.js
@@ -0,0 +1,65 @@
+
+// 1. Declare a variable called username
+
+let userName;
+
+// 2. Declare a variable called myName & assign to it a string 
+// that represents your Full Name.
+
+let myName= "sohial jaffar"
+
+// 3. Write script to
+    // a) Declare a JS variable, titled message.
+    // b) Assign “Hello World” to variable message
+    // c) Display the message in alert box.
+let message="Hello World"
+alert(message)
+
+
+// 4. Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+
+
+const studentData={
+    name:"my name is sohail Jaffar",
+    age:"I am 23 year old",
+    certification:"certified mobile application development"
+}
+
+alert(studentData.name)
+alert(studentData.age)
+alert(studentData.certification)
+
+// 5. Write a script to display the following alert using one JS 
+// variable:
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+
+// 6. Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation)
+
+
+let email="sohail.jaffar@gmail.com"
+alert("My email address is "+ email)
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+let book="A smarter way to learn JavaScript"
+alert("I am trying to learn from the book "+book)
+
+// 8. Write a script to display this in browser through JS
+
+document.querySelector("#topPara").innerHTML="Yah! I can write HTML content through JavaScript"
+
+// 9. Store following string in a variable and show in alert and 
+// browser through JS
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// CHAPTER-------------------4
+
---
+++ chapter4.js
@@ -0,0 +1,19 @@
+let a,b,c;
+
+let page;
+let length;
+let sides;
+let isEmpty;
+let isFull;
+
+// let 2page;
+// let my-books;
+// let abs%length;
+// let var;
+// let alert;
+
+let heading=document.querySelector("h1")
+heading.innerText="Rules for naming JS variables"
+let content=document.querySelector("#para")
+
+content.innerText="Variable name can only contain letters ,numbers, dollar,and underscore \nFor example $my_1stVariable \nVariables must begin with a Dollar, Underscore ,or Letter \nFor example $name, _name or name \nVariable names are case Case sensitive\nVariable names should not be JS JavaScript reserved keywords"
