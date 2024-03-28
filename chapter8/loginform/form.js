25b46a49d9b2268b4302cdb7252c2c2fcd8b722f
sohail786jaffar <sohail.jaffar86@gmail.com>
2024-03-29 00:54:36 +0500

    m

--- chapter8/loginform/form.js
+++ chapter8/loginform/form.js
@@ -1,3 +1,14 @@
-let password1 = document.querySelector(".password1").value
-let password2 = document.querySelector(".password2").value
+// Store the correct password
+var correctPassword = prompt("enter your password"); // Replace with the correct password
 
+// Ask user to enter their password
+var userPassword = prompt('Please re-enter your password');
+
+// Validate the passwords
+if (userPassword === null || userPassword === '') {
+    alert('Please enter your password');
+} else if (userPassword === correctPassword) {
+    alert('Correct! The password you entered matches the original password');
+} else {
+    alert('Incorrect password');
+}
